a4bf42ce5eca7879f7b70f33ed046e3fd1ccce4c
gustavobifani <gustavobifani@gmail.com>
2024-05-28 00:14:29 -0300

    Envio da primeira versão do projeto completa

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vehicles-sprint5
 Repositório referente ao projeto da Sprint 5 do bootcamp de Ciência de dados da Tripleten. Projeto do aluno Gustavo Escuer Bifani.
+
+Temos um diretório chamado "notebooks", onde podemos verificar um caderno do jupyter notebook utilizado para realização de testes de funcionamento de alguns comandos da biblioteca streamlit.
+
+Além disso, temos no nosso diretório principal o conjunto de dados utilizado em toda análise ("vehicles_us.csv"), um arquivo "requiriment.txt" com as bibliotecas utilizadas no nosso projeto, e o código do aplicativo principal "app.py" que contém o código fonte para criação de um site que exibe informações referentes à distância percorrida dos veículos que estão no nosso conjunto de dados: Qual a distribuição de quantidade de veículos com base no valor medido no odômetro e como este valor medido no odômetro se correlaciona com o preço do veículo. Para apresentar esta análise ao usuário do nosso site, contamos com uma breve visão dos dados, uma caixa de seleção na qual o usuário irá selecionar qual análise ele gostaria de observar: Se prefere ver o histograma com a distribuição da quantidade de veículos ou o gráfico de dispersão com a relação preço vs odômetro.
---
+++ app.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import streamlit as st
+import plotly_express as px
+
+car_data = pd.read_csv('vehicles_us.csv')
+
+'''
+Aqui nesses comentários, deixo parte do código que seria utilizado para execução do site usando botões;
+No caso, utilizei a caixa de seleção.
+
+hist_button = st.button('Criar histograma') # criar um botão
+     
+if hist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+         
+    # criar um histograma
+    fig_hist = px.histogram(car_data, x="odometer")
+     
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+scatter_button = st.button('Criar gráfico de dispersão') # criar um botão
+     
+if scatter_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+         
+    # criar um histograma
+    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+     
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_scatter, use_container_width=True)
+'''
+# cabeçalho do nosso site
+st.header('Bem vindo! Aqui neste site você terá acesso as informações de distribuição da quantidade de carros disponíveis no nosso conjunto com base em sua distância percorrida. Além disso, poderá observar de maneira gráfica como isso influencia no preço de cada um deles.')
+
+# DataViewer
+st.write('Este é o cabeçalho do conjunto de dados com os veículos que compõem as análises do nosso site')
+st.dataframe(car_data.head(10))
+
+# Criando nossa caixa de seleção
+build_histogram = st.checkbox('Criar um histograma')
+build_scatter = st.checkbox('Criar um gráfico de dispersão')
+
+if build_histogram:
+    # identificando e detalhando a escolha do usuário na caixa de seleção - histograma
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros, mostrando a distribuição da quantidade de carros de acordo com as informações do odômetro')
+         
+    # criando um histograma com base nas informações de distância total percorrida pelos veículos
+    fig_hist = px.histogram(car_data, x="odometer")
+     
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+if build_scatter:
+    # identificando e detalhando a escolha do usuário na caixa de seleção - gráfico de dispersão
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros, mostrando a correlação entre os preços dos veículos e a distância informada no odômetro')
+         
+    # criando um gráfico de dispersão que correlaciona o preço dos veículos com a distância informada no odômetro
+    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+     
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_scatter, use_container_width=True)    
